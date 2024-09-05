ae8f6ed0806f658812e9f9d59b6b6d27f7ab83f0
Sakar Dalal <165682955+SakarDalal@users.noreply.github.com>
2024-09-05 20:59:29 +0530

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    // Animate elements on scroll
+    $(window).on('scroll', function () {
+        $('.fade-in').each(function () {
+            var elementPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elementPos < topOfWindow + $(window).height() - 100) {
+                $(this).addClass('animate__animated animate__fadeIn');
+            }
+        });
+    });
+
+    // Smooth scroll for anchor links
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 70 // Adjust for navbar height
+            }, 1000);
+        }
+    });
+});
+
+
